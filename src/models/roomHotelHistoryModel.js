2225727cd78a35fd8d736651a4783e3eed955324
Aleixs-Trejo <66097331+Aleixs-Trejo@users.noreply.github.com>
2024-08-13 16:14:52 -0500

    borrando archivos inecesarios room-hotel

--- src/models/roomHotelHistoryModel.js
+++
@@ -1,6 +0,0 @@
-const {model} = require("mongoose");
-const roomHotelHistorySchema = require("./RoomHotelHistory");
-
-const roomHotelHistoryModel = model("RoomHotelHistory", roomHotelHistorySchema);
-
-module.exports = roomHotelHistoryModel;
